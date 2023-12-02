340ae624de4415f300cc69c67ad1551798bb91ab
karim-elmogy <kimo72636@gmail.com>
2023-12-02 21:31:24 +0200

    fkgfg

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.index');
+    return view('user.stop');
 });
 
 Route::resource('sliders', SliderController::class);
